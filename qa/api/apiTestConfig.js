fb4389732221fc0bd26c367cddd2ea1afabfcfb6
diablo <diablo@localhost.localdomain>
2014-09-23 21:10:44 -0500

    Allows testing of prod

--- qa/api/apiTestConfig.js
+++ qa/api/apiTestConfig.js
@@ -1,9 +1,15 @@
 var config = {},
     util = require('util'),
     port = process.env.PORT || '5000',
+    environment = process.env.ENV || 'local',
     request = require('request-json');
 
-console.log("Using port: " + port);
-config.rootUrl = util.format('http://localhost:%s/api', port);
+if (environment === 'PROD') {
+    config.rootUrl = 'http://fiveyear.herokuapp.com/api';
+} else {
+    console.log("Using port: " + port);
+    config.rootUrl = util.format('http://localhost:%s/api', port);
+}
         
+console.log("Using rootUrl: " + config.rootUrl);
 module.exports = config;
